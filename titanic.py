9b3e1710d77d24f2dd9e7568349ac274617a72e8
vineet <vs738710@gmail.com>
2025-08-15 23:50:40 +0530

    try

---
+++ titanic.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.model_selection import KFold, StratifiedKFold, cross_val_score
+import matplotlib.pyplot as plt
+
